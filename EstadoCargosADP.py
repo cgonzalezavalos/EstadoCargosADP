b5e133d79a8a0d36d00029eb557b9c9e0103e2f2
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-03-20 10:55:10 -0300

    carga datos excel, crea filtros y muestra data.head(10)

--- EstadoCargosADP.py
+++ EstadoCargosADP.py
@@ -25,3 +25,45 @@ st.markdown(
 
 # Add horizontal line
 st.markdown("<hr>", unsafe_allow_html=True)
+
+# carga de datos
+data=pd.read_excel('datos/datosEstadoCargosADP.xlsx',sheet_name='datos')
+
+#------------------------------------------------------------------------
+# listas valores filtros
+#------------------------------------------------------------------------
+unique_sexo = data['Sexo'].unique()
+Sexo = pd.DataFrame({'Sexo': unique_sexo})
+nuevo_registro = pd.DataFrame({'Sexo': ['Todos']})
+rango_etario = pd.concat([nuevo_registro, Sexo])
+Sexo = Sexo.reset_index(drop=True)
+Sexo = Sexo['Sexo'].tolist()
+
+unique_region = data['Region'].unique()
+region = pd.DataFrame({'Region': unique_region})
+nuevo_registro = pd.DataFrame({'Region': ['Todas']})
+region = pd.concat([nuevo_registro, region])
+region = region.reset_index(drop=True)
+region = region['Region'].tolist()
+
+unique_ministerio = data['Ministerio'].unique()
+Ministerio = pd.DataFrame({'Ministerio': unique_ministerio})
+nuevo_registro = pd.DataFrame({'Ministerio': ['Todos']})
+Ministerio = pd.concat([nuevo_registro, Ministerio])
+Ministerio = Ministerio.reset_index(drop=True)
+Ministerio = Ministerio['Ministerio'].tolist()
+
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+# Filtros
+with st.container():
+    col1,col2,col3=st.columns(3)
+    with col1:
+        option1=st.selectbox('Ministerio',Ministerio)
+    with col2:
+        option2=st.selectbox('Región',region)
+    with col3:
+        option3=st.selectbox('Sexo', Sexo)
+
+with st.container():
+    st.dataframe(data.head(10))
