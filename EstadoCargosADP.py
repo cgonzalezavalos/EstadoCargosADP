bab5129891d76a0a63a0980c9a32458e00ffd922
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-03-20 14:20:26 -0300

    sidebar

--- EstadoCargosADP.py
+++ EstadoCargosADP.py
@@ -35,10 +35,18 @@ st.caption(f'Fecha de actualización: _{fecha_actualizacion}_')
 # Add horizontal line
 st.markdown("<hr>", unsafe_allow_html=True)
 
-
 # colores y orden de los estados
 Estados_Orden=['Planificación','Convocatoria','En Evaluación','Nómina','Nombrado','Titular No ADP']
 Estado_Color = {'Planificación': 'orange', 'Convocatoria': 'blue','En Evaluación':'grey','Nómina':'yellow','Nombrado':'#00f118','Titular No ADP':'red'}  # Mapeo de colores por sexo
+
+
+with st.sidebar:
+    Genero = st.radio(
+        "Ver estadisticas de cargos nombrados desagregados por sexo",("Si", "No")
+    )
+    Detalle = st.radio('Ver detalle de cargos nombrados',("Si", "No"))
+
+
 
 #------------------------------------------------------------------------
 # listas valores filtros
